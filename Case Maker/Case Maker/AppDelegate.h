8b3315db13b54c662c1d500f7f9253b161db5159
Matthew Appleby <appleby.matthew@gmail.com>
2014-08-08 19:10:07 -0700

    Created Xcode Project

---
+++ Case Maker/Case Maker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Case Maker
+//
+//  Created by Matt Appleby on 8/8/14.
+//  Copyright (c) 2014 Zazzle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Case Maker/Case Maker/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Case Maker
+//
+//  Created by Matt Appleby on 8/8/14.
+//  Copyright (c) 2014 Zazzle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
