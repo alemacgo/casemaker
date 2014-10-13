2bde46623080cf45dfcae2a480a1b94cb2c974a6
Alejandro Machado <alemacgo@gmail.com>
2014-10-13 18:45:38 +0100

    Refactor setting of default attributes for CircleButton

--- ZPatterns/CircleButton.swift
+++ ZPatterns/CircleButton.swift
@@ -9,23 +9,27 @@
 import UIKit
 
 class CircleButton: UIButton {
-
     required init(coder: NSCoder) {
-        //TODO: Remove magic numbers
+        // Called from the storyboards
         super.init(coder: coder)
-        self.backgroundColor = UIColor(hue: 1.0, saturation: 0.5, brightness: 1.0, alpha: 1.0)
-        
-        self.layer.cornerRadius = self.frame.size.width / 2
-        self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0).CGColor
-        self.layer.shadowOpacity = 0.5
-        self.layer.shadowOffset = CGSizeMake(0, 1)
-        self.layer.shadowRadius = 2
+        setDefaultAttributes()
     }
     
     override init(frame: CGRect) {
+        // Called from code
         super.init(frame: frame)
+        setDefaultAttributes()
+    }
+    
+    init(frame: CGRect, color: UIColor) {
+        super.init(frame: frame)
+        self.backgroundColor = color
+    }
+    
+    func setDefaultAttributes() {
+        //TODO: Remove magic numbers
         self.backgroundColor = UIColor(hue: 1.0, saturation: 0.5, brightness: 1.0, alpha: 1.0)
-        
+    
         self.layer.cornerRadius = self.frame.size.width / 2
         self.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1.0).CGColor
         self.layer.shadowOpacity = 0.5
